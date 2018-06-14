9d0ee5a66822a00e9167f579296492d67a04a9dd
Bianca Amorim <biancamoriim@gmail.com>
2018-06-14 20:41:27 -0300

    final commit

--- face_eye_detector.py
+++ face_eye_detector.py
@@ -4,7 +4,7 @@ import cv2
 classificadorFace = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 classificadorOlhos = cv2.CascadeClassifier('cascades/haarcascade_eye.xml')
 
-imagem = cv2.imread('pessoas/pessoas1.jpg')
+imagem = cv2.imread('pessoas/pessoas3.jpg')
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
 facesDetectadas = classificadorFace.detectMultiScale(imagemCinza)
--- facial_detector.py
+++ facial_detector.py
@@ -2,7 +2,21 @@ import cv2
 
 classificador = cv2.CascadeClassifier('cascades\haarcascade_frontalface_default.xml')
 
-imagem = cv2.imread('pessoas/pessoas4.jpg')
+#imagem = cv2.imread('pessoas/beatles.jpg')
+#imagem = cv2.imread('pessoas/faceolho.jpg')
+#imagem = cv2.imread('pessoas/pessoas1.jpg')
+#imagem = cv2.imread('pessoas/pessoas2.jpg')
+#imagem = cv2.imread('pessoas/pessoas3.jpg')
+#imagem = cv2.imread('pessoas/pessoas4.jpg')
+#imagem = cv2.imread('pessoas/escritorio.jpg')
+#imagem = cv2.imread('pessoas/extraterreste.png')
+#imagem = cv2.imread('pessoas/muitagente.jpg')
+#imagem = cv2.imread('pessoas/muitagente2.jpg')
+#imagem = cv2.imread('pessoas/muitagente3.jpg')
+#imagem = cv2.imread('pessoas/people.png')
+imagem = cv2.imread('pessoas/prince.jpg')
+
+
 
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
